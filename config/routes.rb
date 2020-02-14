53731e50a6164d00f48a050a0c22637d2ac573a8
kaori hayashi <suzu_33@hotmail.com>
2020-02-14 20:54:41 +0900

    投稿ページのcss完成

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,6 @@
 #                           PUT    /entries/:id(.:format)                                                                   entries#update
 #                           DELETE /entries/:id(.:format)                                                                   entries#destroy
 #                   replies POST   /replies(.:format)                                                                       replies#create
-#                edit_reply GET    /replies/:id/edit(.:format)                                                              replies#edit
 #                     reply DELETE /replies/:id(.:format)                                                                   replies#destroy
 #                     goods POST   /goods(.:format)                                                                         goods#create
 #                      good DELETE /goods/:id(.:format)                                                                     goods#destroy
@@ -30,11 +29,19 @@
 #                reply_good DELETE /reply_goods/:id(.:format)                                                               reply_goods#destroy
 #                 bookmarks POST   /bookmarks(.:format)                                                                     bookmarks#create
 #                  bookmark DELETE /bookmarks/:id(.:format)                                                                 bookmarks#destroy
+#         letter_opener_web        /inbox                                                                                   LetterOpenerWeb::Engine
 #        rails_service_blob GET    /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
 # rails_blob_representation GET    /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
 #        rails_disk_service GET    /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
 # update_rails_disk_service PUT    /rails/active_storage/disk/:encoded_token(.:format)                                      active_storage/disk#update
 #      rails_direct_uploads POST   /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
+# 
+# Routes for LetterOpenerWeb::Engine:
+# clear_letters DELETE /clear(.:format)                 letter_opener_web/letters#clear
+# delete_letter DELETE /:id(.:format)                   letter_opener_web/letters#destroy
+#       letters GET    /                                letter_opener_web/letters#index
+#        letter GET    /:id(/:style)(.:format)          letter_opener_web/letters#show
+#               GET    /:id/attachments/:file(.:format) letter_opener_web/letters#attachment
 
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :edit, :update, :show] do
