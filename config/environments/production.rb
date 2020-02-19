863a8c11e57becf459410cc1734155c7d976dd4e
kaori hayashi <suzu_33@hotmail.com>
2020-02-19 20:41:22 +0900

    actionmailer修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # アクションメーラー関連
-  config.action_mailer.default_url_options = { host: 'agile-fortress-57225.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'agile-fortress-57225.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
