3e1a4157888e0792c1aaebd81da38f6b2c869915
kaori hayashi <suzu_33@hotmail.com>
2020-02-20 23:34:26 +0900

    バリデーションのカラム名日本語化、略語変数名を修正

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,12 +2,12 @@ class BookmarksController < ApplicationController
   before_action :not_logged_in
 
   def create
-    bm = current_user.bookmarks.create(entry_id: params[:entry_id])
+    bookmark = current_user.bookmarks.create(entry_id: params[:entry_id])
     redirect_to entries_path
   end
 
   def destroy
-    bm = current_user.bookmarks.find_by(id: params[:id]).destroy
+    bookmark = current_user.bookmarks.find_by(id: params[:id]).destroy
     redirect_to entries_path
   end
 end
